31babe8043989235ad1db9b8f9e671fe0bedecef
Emily Morgan <emorgan05@gmail.com>
2017-09-25 13:33:07 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,11 +28,11 @@ def monopoly_with_third_tier
 			:pieces => 4,
 			:names => {},
 			:rent_in_dollars => {
-				:one_piece_owned => 25
+				:one_piece_owned => 25,
+				:two_pieces_owned => 50,
 			}
 		}
 	}
-
 end
 
 def monopoly_with_fourth_tier
